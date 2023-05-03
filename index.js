1166df77ed8876d604de3010b4d23a08f4864d7f
BRYUSS <zubaydullamurodullayev4@gmail.com>
2023-05-03 22:11:48 +0500

    yukla

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const express = require("express");
+const app = express();
 
+app.get('/topish', (req , res)=>{
+ res.send(`ufouf`)
+})
 
-
-
+app.listen(3002,()=>{
+console.log("backend ishladi");
+});
 
